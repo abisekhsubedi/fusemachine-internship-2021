127d1ace883ca579b68da0ce7f9f351e547038b5
=abisekh subedi <=20342543+abisekhsubedi@users.noreply.github.com>
2021-05-14 12:40:27 +0545

    Add responsiveness to product display cards

--- webpack.config.js
+++ webpack.config.js
@@ -9,111 +9,102 @@ const webpack = require('webpack');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = (env) => {
-  const isProduction = env.production ? true : false;
-  const envType = isProduction ? 'production' : 'development';
+    const isProduction = env.production ? true : false;
+    const envType = isProduction ? 'production' : 'development';
 
-  return {
-    mode: envType,
-    entry: ['./src/app.js'],
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'js/script.js'
-    },
-    devServer: {
-      contentBase: './dist',
-      hot: true
-    },
-    /*Loaders*/
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: 'css-loader'
+    return {
+        mode: envType,
+        entry: ['./src/app.js'],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/script.js'
         },
-        {
-          test: /\.scss$/,
-          use: [
-            // fallback to style-loader in development
-            !isProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader'
-          ]
+        devServer: {
+            contentBase: './dist',
+            hot: true
         },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]?[hash]',
-                outputPath: 'images/',
-                publicPath: isProduction ? '../images/' : ''
-              }
-            }
-          ]
+        /*Loaders*/
+        module: {
+            rules: [{
+                    test: /\.css$/,
+                    use: 'css-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        // fallback to style-loader in development
+                        !isProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]?[hash]',
+                            outputPath: 'images/',
+                            publicPath: isProduction ? '../images/' : ''
+                        }
+                    }]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: '/node_modules/',
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]?[hash]',
+                            outputPath: 'fonts/',
+                            publicPath: isProduction ? '../fonts/' : ''
+                        }
+                    }]
+                }
+            ]
         },
-        {
-          test: /\.js$/,
-          exclude: '/node_modules/',
-          use: ['babel-loader']
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]?[hash]',
-                outputPath: 'fonts/',
-                publicPath: isProduction ? '../fonts/' : ''
-              }
-            }
-          ]
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].css'
+            }),
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: './src/templates/index.html',
+                title: 'Scalable Sass Architecture'
+            }),
+            new CopyWebpackPlugin([{
+                from: 'src/templates',
+                to: ''
+            }]),
+            new ImageminPlugin({
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                disable: !isProduction, // Disable during development
+                pngquant: {
+                    quality: '80-100'
+                }
+            }),
+            new CopyWebpackPlugin([{
+                from: 'src/images',
+                to: 'images/[path][name].[ext]'
+            }])
+        ],
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    uglifyOptions: {
+                        keep_classnames: true,
+                        warnings: false
+                    }
+                }),
+                new OptimizeCSSAssetsPlugin({})
+            ]
         }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'css/[name].css'
-      }),
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: './src/templates/index.html',
-        title: 'Scalable Sass Architecture'
-      }),
-      new CopyWebpackPlugin([
-        {
-          from: 'src/templates',
-          to: ''
-        }
-      ]),
-      new ImageminPlugin({
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        disable: !isProduction, // Disable during development
-        pngquant: {
-          quality: '80-100'
-        }
-      }),
-      new CopyWebpackPlugin([
-        {
-          from: 'src/images',
-          to: 'images/[path][name].[ext]'
-        }
-      ])
-    ],
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin({
-          cache: true,
-          parallel: true,
-          uglifyOptions: {
-            keep_classnames: true,
-            warnings: false
-          }
-        }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    }
-  };
+    };
 };
